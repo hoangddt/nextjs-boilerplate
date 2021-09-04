d8b3528c7bd4e2ad4712daa8194637a0375e646b
hoangddt <quochoangddt@gmail.com>
2021-09-04 21:18:58 +0700

    add: javascript base with tailwind

---
+++ src/pages/tailwind.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+
+export default function TailWind() {
+    return (
+        <div className="py-12 bg-blue-100 md:py-24">
+            <div className="grid max-w-screen-xl px-6 mx-auto lg:px-8 xl:px-4 md:grid-cols-4 xl:grid-cols-5 gap-x-12 lg:gap-x-20">
+                <h4>This is center</h4>
+            </div>            
+        </div>
+    )
+}
